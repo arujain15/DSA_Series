5fff3206c4a041353fe16989b597aa6c9a59ec7d
Arushi Jain <jarushi911@gmail.com>
2026-03-01 21:36:56 +0530

    Day -14 : Basic Paranthesis - day 2 stack, d-1 understanding stack

---
+++ BasicParanthesis.java
@@ -0,0 +1,43 @@
+package com.arushi.stackd1;
+
+import java.util.Stack;
+
+public class BasicParanthesis {
+
+	public static void main(String[] args) {
+		String s = "(){}[]";
+		System.out.println(new BasicParanthesis().isValid(s));
+	}
+	
+	public boolean isValid(String s)
+	{
+		if(s == null || s.length() == 0)
+		{
+			return true;
+		}
+		Stack<Character> stack = new Stack<>();
+
+		for(int i = 0; i < s.length(); i++)
+		{
+			char ch = s.charAt(i);
+			
+			if(ch == '(' || ch == '{' || ch == '[')
+			{
+				stack.push(ch);
+			}
+			else
+			{
+				if(stack.isEmpty())
+					return false;
+				char top = stack.pop();
+				
+				if((ch == ')' && top != '(') || (ch ==']' && top != '[') || (ch == '}' && top != '{'))
+					return false;
+			}
+			
+			
+		}
+		return stack.isEmpty();
+	}
+
+}
